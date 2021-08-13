072221b9718522006b93ca393bacf5c0a02dc03c
Robert <bates.to.infinity@gmail.com>
2021-08-12 21:10:27 -0500

    commit tree 401cbdee0b57e7b3d6a9a9a01f30e04353b233b2

--- app.rb
+++ app.rb
@@ -2,11 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		"<h1>Hello world</h1>"		
-	end
+	#get '/' do
+		#"<h1>Hello world</h1>"		
+	#end
 
 	get '/' do
 		erb :index
+	end
+
+	get '/info' do
+		erb :info
 	end
 end
